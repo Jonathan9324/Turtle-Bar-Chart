80de0d9989c3f39c4486e27842cd8a5bd2593021
Jonathan9324 <jonathanbm72@gmail.com>
2024-07-18 09:41:50 +1200

    Add files via upload

--- Bar Charts with a Turtle.py
+++ Bar Charts with a Turtle.py
@@ -108,21 +108,7 @@ bars()
 #turtle.onscreenclick(ChartSetup)
 col = ['red', 'yellow', 'green', 'blue', 
        'white', 'black', 'orange', 'pink'] 
-  
-# method to call on screen click 
-  
-  
-def fxn(x, y): 
-    global col 
-    ind = random.randint(0, 7) 
-      
-    # set screen color randomly 
-    sc.bgcolor(col[ind]) 
-  
-# set screen 
-#sc = turtle.Screen() 
-#sc.setup(400, 300) 
-  
+
 # call method on screen click 
 wn.onscreenclick(ChartSetup)
 turtle.done()
